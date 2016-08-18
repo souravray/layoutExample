0c8033fc290fe3182bdae296e6e1a283f2e49343
Sourav Ray <me@raysourav.com>
2016-08-19 04:19:24 +0530

    Initial Commit

---
+++ textLayout/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  textLayout
+//
+//  Created by Sourav Ray on 8/19/16.
+//  Copyright © 2016 Sourav Ray. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ textLayout/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  textLayout
+//
+//  Created by Sourav Ray on 8/19/16.
+//  Copyright © 2016 Sourav Ray. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
